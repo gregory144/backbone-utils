db9014d1afb3e711cd09554d4a0addebd83a8a06
Greg Gross <greg@gtgross.com>
2012-03-06 18:27:59 +1100

    initial version

---
+++ backbone-automodel.js
@@ -0,0 +1,17 @@
+(function($){
+
+    window.Backbone.AutoModel = Backbone.Model.extend({
+        getForTemplate: function(attribute) {
+            var autoRenderId = _.uniqueId('auto-render-');
+            var el = null;
+            this.bind('change', function() {
+                if (!el) {
+                    el = $('.' + autoRenderId);
+                }
+                el.text(this.get(attribute));
+            }, this);
+            return "<span class='" + autoRenderId + "'>" + this.get(attribute) + "</span>";
+        }
+    });
+
+})(jQuery);
